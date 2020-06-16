2fe15cd7c1d5f12714a49eddb2ebb99c22568883
Oswald Calvario <oswald.calvario@gmail.com>
2020-06-16 06:30:31 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -18,9 +18,7 @@ class App < Sinatra::Base
     "Goodbye, #{@user_name}."
   end
   
-get "/multiply/:num1/:num2" do
+  get "/multiply/:num1/:num2" do
 		"#{params[:num1].to_i*params[:num2].to_i}"
-	end
-end
-
+	  end
 end
